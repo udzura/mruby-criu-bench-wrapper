27b66c4746b5b446ab49c90f150204e054a20876
Uchio Kondo <udzura@udzura.jp>
2019-09-26 13:30:07 +0900

    Created

--- mrbgem.rake
+++ mrbgem.rake
@@ -2,8 +2,12 @@ MRuby::Gem::Specification.new('mruby-criu-bench-wrapper') do |spec|
   spec.bins    = ['criu']
 
   spec.add_dependency 'mruby-io', core: 'mruby-io'
+  spec.add_dependency 'mruby-socket', mgem: 'mruby-socket'
   spec.add_dependency 'mruby-metaprog', core: 'mruby-metaprog'
+  spec.add_dependency 'mruby-env', mgem: 'mruby-env'
+  spec.add_dependency 'mruby-exec', github: 'haconiwa/mruby-exec'
   spec.add_dependency 'mruby-fibered_worker', github: 'udzura/mruby-fibered_worker'
+  spec.add_dependency 'mruby-syslog', github: 'udzura/mruby-syslog'
 
   spec.license = 'MIT'
   spec.authors = 'Uchio Kondo'
--- mrblib/mrb_criubenchwrapper.rb
+++ mrblib/mrb_criubenchwrapper.rb
@@ -1,5 +1,32 @@
-class CriuBenchWrapper
-  def bye
-    self.hello + " bye"
+def __main__(argv)
+  Syslog.open("haconiwa.criu-wrapper")
+  Syslog.info("CRIU is booting via criu-wrapper")
+
+  bin = nil
+  %w[/usr/local/sbin/criu /usr/local/bin/criu /usr/sbin/criu /usr/bin/criu].each do |binpath|
+    if File.exist?(binpath) && File.readlink("/proc/self/exe") != binpath
+      bin = binpath
+    end
   end
+  bin = ENV['CRIU_BIN_PATH'] if ENV['CRIU_BIN_PATH']
+
+  loop = FiberedWorker::MainLoop.new
+
+  main = Process.fork do
+    Exec.execv bin, *argv
+  end
+  tpid = Process.fork do
+    # customize me
+    system "timeout -s INT 2s strace -c -f -p #{main} > /tmp/strace.#{$$}.log 2>&1"
+  end
+
+  Syslog.info "Main is: #{main}"
+  loop.pids = [main, tpid]
+  loop.on_worker_exit do |status, rest|
+    Syslog.info "Termed process! #{status.inspect} rest: #{rest.inspect}"
+  end
+  ret = loop.run
+
+  Syslog.info("Finished: #{ret.inspect}")
+  Syslog.close
 end
